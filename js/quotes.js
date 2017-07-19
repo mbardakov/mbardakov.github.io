cda1a41bda2b61a46ec6153d8c135aaf314fd71e
mbardakov <mbardakov@gmail.com>
2017-07-18 22:15:48 -0400

    preliminary quote-injection code

---
+++ js/quotes.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var quotebox = document.getElementById("intro-quote");
+    quotebox.innerText = "You're looking lovely today (:";
+    // come up with quotes to put here
+    // choose one with RNG
+};
